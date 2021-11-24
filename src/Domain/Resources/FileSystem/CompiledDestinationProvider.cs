2919368de721f8876469ee368072753a7958c683
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2021-11-25 00:30:01 +0100

    CompiledDestinationProvider: readability refactor

--- src/Domain/Resources/FileSystem/CompiledDestinationProvider.cs
+++ src/Domain/Resources/FileSystem/CompiledDestinationProvider.cs
@@ -32,9 +32,11 @@ namespace Scrap.Resources.FileSystem
             return result;
         }
 
-        public Task<string> GetDestinationAsync(string destinationRootFolder,
+        public Task<string> GetDestinationAsync(
+            string destinationRootFolder,
             Page page,
-            int pageIndex, Uri resourceUrl,
+            int pageIndex,
+            Uri resourceUrl,
             int resourceIndex)
         {
             return _compiledDestinationProvider.GetDestinationAsync(destinationRootFolder, page, pageIndex, resourceUrl, resourceIndex);
@@ -42,72 +44,87 @@ namespace Scrap.Resources.FileSystem
 
         private void Compile()
         {
-            string sourceCode = "";
+            string sourceCode = GenerateSourceCode();
             try
             {
-                _logger.LogDebug("Compiling destination expression...");
-            
-                var directoryName = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? throw new Exception("Cannot find entry assembly path");
-                var sourcePath = Path.Combine(directoryName, @"Resources\FileSystem\TemplateDestinationProvider.cs");
-
-                // define source code, then parse it (to the type used for compilation)
-                sourceCode = File.ReadAllText(sourcePath);
-                var pattern = "destinationRootFolder" + string.Join("", _destinationFolderPattern.Select(p => $".C({p})")) + ".ToPath()";
-                sourceCode = sourceCode.Replace("\"destinationFolderPattern\"", pattern);
-                SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
-
-                // define other necessary objects for compilation
-                string assemblyName = Path.GetRandomFileName();
-
-                var references = ReferenceAssemblies.Net50.Concat(new[]
-                {
-                    MetadataReference.CreateFromFile(typeof(IDestinationProvider).Assembly.Location),
-                    MetadataReference.CreateFromFile(typeof(HtmlDocument).Assembly.Location),
-                }).ToArray();
-
-                // analyse and generate IL code from syntax tree
-                CSharpCompilation compilation = CSharpCompilation.Create(
-                    assemblyName,
-                    syntaxTrees: new[] { syntaxTree },
-                    references: references,
-                    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-                using var ms = new MemoryStream();
-                // write IL code into memory
-                EmitResult result = compilation.Emit(ms);
-
-                if (!result.Success)
-                {
-                    // handle exceptions
-                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
-                        diagnostic.IsWarningAsError ||
-                        diagnostic.Severity == DiagnosticSeverity.Error);
-
-                    foreach (Diagnostic diagnostic in failures)
-                    {
-                        _logger.LogError("{Id}: {Message} at {Location}", diagnostic.Id, diagnostic.GetMessage(),
-                            diagnostic.Location);
-                    }
-
-                    throw new Exception("Compilation error");
-                }
-
-                // load this 'virtual' DLL so that we can use
-                ms.Seek(0, SeekOrigin.Begin);
-                Assembly assembly = Assembly.Load(ms.ToArray());
-
-                // create instance of the desired class and call the desired function
-                var typeName = "Scrap.Resources.FileSystem.TemplateDestinationProvider";
-                Type type = assembly.GetType(typeName) ?? throw new Exception($"Type {typeName} not found");
-                object obj = Activator.CreateInstance(type) ?? throw new Exception("Could not activate instance");
-
-                _compiledDestinationProvider = (IDestinationProvider)obj;
+                var assembly = CompileSourceCode(sourceCode);
+                _compiledDestinationProvider = CreateDestinationProviderInstance(assembly);
             }
             catch (Exception)
             {
                 _logger.LogDebug("Source: {SourceCode}", sourceCode);
                 throw;
             }
+        }
+
+        private string GenerateSourceCode()
+        {
+            var directoryName = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ??
+                                throw new Exception("Cannot find entry assembly path");
+            var sourcePath = Path.Combine(directoryName, @"Resources\FileSystem\TemplateDestinationProvider.cs");
+
+            // define source code, then parse it (to the type used for compilation)
+            string sourceCode = File.ReadAllText(sourcePath);
+            var pattern = "destinationRootFolder" + string.Join("", _destinationFolderPattern.Select(p => $".C({p})")) +
+                          ".ToPath()";
+            sourceCode = sourceCode.Replace("\"destinationFolderPattern\"", pattern);
+            return sourceCode;
+        }
+
+        private Assembly CompileSourceCode(string sourceCode)
+        {
+            _logger.LogDebug("Compiling destination expression...");
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+            // define other necessary objects for compilation
+            string assemblyName = Path.GetRandomFileName();
+
+            var references = ReferenceAssemblies.Net50.Concat(new[]
+            {
+                MetadataReference.CreateFromFile(typeof(IDestinationProvider).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(HtmlDocument).Assembly.Location),
+            }).ToArray();
+
+            // analyse and generate IL code from syntax tree
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using var ms = new MemoryStream();
+            // write IL code into memory
+            EmitResult result = compilation.Emit(ms);
+
+            if (!result.Success)
+            {
+                // handle exceptions
+                IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+
+                foreach (Diagnostic diagnostic in failures)
+                {
+                    _logger.LogError("{Id}: {Message} at {Location}", diagnostic.Id, diagnostic.GetMessage(),
+                        diagnostic.Location);
+                }
+
+                throw new Exception("Compilation error");
+            }
+
+            // load this 'virtual' DLL so that we can use
+            ms.Seek(0, SeekOrigin.Begin);
+            Assembly assembly = Assembly.Load(ms.ToArray());
+            return assembly;
+        }
+
+        private static IDestinationProvider CreateDestinationProviderInstance(Assembly assembly)
+        {
+            var typeName = "Scrap.Resources.FileSystem.TemplateDestinationProvider";
+            Type type = assembly.GetType(typeName) ?? throw new Exception($"Type {typeName} not found");
+            object obj = Activator.CreateInstance(type) ?? throw new Exception("Could not activate instance");
+
+            return (IDestinationProvider)obj;
         }
     }
 }

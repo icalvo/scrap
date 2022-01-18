591a3006a1d3847532f5bb5cf5c7f7041d824fd6
Ignacio Calvo <ignacio.calvo@gmail.com>
2022-01-18 19:39:34 +0100

    Refactors

--- src/Domain/HtmlDocumentExtensions.cs
+++ src/Domain/HtmlDocumentExtensions.cs
@@ -1,27 +1,39 @@
-﻿using System.Xml.XPath;
+﻿using System.Collections;
+using System.Xml.XPath;
 using HtmlAgilityPack;
 
 namespace Scrap;
 
 public static class HtmlDocumentExtensions
 {
-        
     public static IEnumerable<string?> Contents(this HtmlDocument doc, XPath xpath)
     {
-        XPathNavigator nav = doc.CreateNavigator() ?? throw new Exception();
-        var sel3 = nav.Select(xpath);
-        if (xpath.IsHtml)
+        XPathNavigator nav = doc.CreateNavigator()
+                             ?? throw new ArgumentException("Could not create XPathNavigator", nameof(doc));
+        var childNodes = nav.Select(xpath).ToEnumerable();
+        
+        return xpath.IsHtml
+            ? childNodes.Select(node => node.InnerXml)
+            : childNodes.Select(node => node.Value);
+    }
+
+    /// <summary>
+    /// Converts an <see cref="XPathNodeIterator"/> to a <see cref="IEnumerable{T}" />.
+    /// </summary>
+    /// <param name="iterator">Iterator to convert.</param>
+    /// <returns></returns>
+    /// <remarks>
+    /// <see cref="XPathNodeIterator"/> is an old, pre-generics class which has MoveNext() and Current: T
+    /// (without implementing <see cref="IEnumerator{T}"/>) and implements <see cref="IEnumerable"/>.
+    /// This method will convert it to a modern <see cref="IEnumerable{T}"/>.
+    /// </remarks>
+    private static IEnumerable<XPathNavigator> ToEnumerable(this XPathNodeIterator iterator)
+    {
+        while (iterator.MoveNext())
         {
-            while (sel3.MoveNext())
+            if (iterator.Current != null)
             {
-                yield return sel3.Current?.InnerXml;
-            }
-        }
-        else
-        {
-            while (sel3.MoveNext())
-            {
-                yield return sel3.Current?.Value;
+                yield return iterator.Current;
             }
         }
     }

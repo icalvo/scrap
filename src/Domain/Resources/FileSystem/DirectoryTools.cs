cd4cf88de883b8cefc4d1dcf495212334f0e95c5
Ignacio Calvo <ignacio.calvo@gmail.com>
2023-03-24 14:27:49 +0100

    Fixed Dir.CreateIfNotExistAsync bug

--- src/Domain/Resources/FileSystem/DirectoryTools.cs
+++ src/Domain/Resources/FileSystem/DirectoryTools.cs
@@ -11,8 +11,10 @@ public class DirectoryTools
 
     public async Task CreateIfNotExistAsync(string path)
     {
-        if (await _fileSystem.DirectoryExistsAsync(path))
+        if (!await _fileSystem.DirectoryExistsAsync(path))
+        {
             await _fileSystem.DirectoryCreateAsync(path);
+        }
     }
 
     public Task CreateAsync(string path) => _fileSystem.DirectoryCreateAsync(path);

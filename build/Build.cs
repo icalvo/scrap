10d9c8b4289ab6f6a3cd22bbc91be9de6b77f5e5
Ignacio Calvo <ignacio.calvo@gmail.com>
2023-09-20 21:11:16 +0200

    Small fixes in nuke build

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,15 @@
 using System;
-using System.IO;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using Octokit;
 using Serilog;
-using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [ShutdownDotNetAfterServerBuild]
@@ -32,6 +30,7 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
     InvokedTargets = new[] { nameof(PullRequest) },
     EnableGitHubToken = true,
     ImportSecrets = new[] { nameof(NugetToken) })]
+[SuppressMessage("ReSharper", "AllUnderscoreLocalParameterName")]
 class Build : NukeBuild
 {
     const string ChangelogFileName = "CHANGELOG.md";
@@ -40,7 +39,6 @@ class Build : NukeBuild
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [CI] readonly GitHubActions GitHubActions;
-    [GitRepository] readonly GitRepository GitRepository;
 
     [Parameter] [Secret] readonly string NugetToken;
 
@@ -73,7 +71,7 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(dir => dir.DeleteDirectory());
         });
 
     Target Restore => _ => _
@@ -144,7 +142,7 @@ class Build : NukeBuild
         .Produces(PackageDirectory / "*.nupkg")
         .Executes(() =>
         {
-            EnsureCleanDirectory(PackageDirectory);
+            PackageDirectory.CreateOrCleanDirectory();
             DotNetPack(s => s
                 .SetProject(TargetProjectDirectory)
                 .SetConfiguration(Configuration)
@@ -158,10 +156,11 @@ class Build : NukeBuild
         .Requires(() => Version)
         .Executes(() =>
         {
-            Assert.FileExists(RootDirectory / ChangelogFileName);
+            AbsolutePath changelog = RootDirectory / ChangelogFileName;
+            Assert.FileExists(changelog);
             Assert.True(
-                File.ReadLines(RootDirectory / ChangelogFileName).Any(line => line.StartsWith($"## [{MainVersion}")),
-                $"There is no entry for version {Version} in {ChangelogFileName}");
+                changelog.ReadAllLines().Any(line => line.StartsWith($"## [{MainVersion}]")),
+                $"There is no entry for version {MainVersion} in {ChangelogFileName}");
         });
 
     public Target Push => _ => _
